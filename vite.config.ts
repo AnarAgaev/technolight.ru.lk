fe695cd369ecd3fcb5063e4a22a5a78110fa0e32
AnarAgaev <anar.n.agaev@gmail.com>
2025-10-08 12:11:15 +0300

    Added vite-bundle-analyzer to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		analyzer(),
+	],
+	build: {
+		cssCodeSplit: false,
+		rollupOptions: {
+			output: {
+				// Убираем hash из имен файлов для entry points
+				entryFileNames: `assets/[name].js`,
+				// Убираем hash из имен файлов для assets (CSS, изображения и т.д.)
+				assetFileNames: `assets/[name].[ext]`,
+				// Убираем hash из имен файлов для динамически загружаемых модулей (chunks)
+				chunkFileNames: `assets/[name].js`,
+			},
+		},
+	},
 })
